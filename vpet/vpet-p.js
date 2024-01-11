7f636a8647fde2695659dd3f1d10c976b38d9926
inkphyme <98397983+inkosamu@users.noreply.github.com>
2024-01-11 15:48:27 +0800

    Add files via upload

---
+++ vpet/vpet-p.js
@@ -0,0 +1,8 @@
+var timer;
+            function  autoPlay(){
+                     timer = setInterval(function(){
+                           nextImg(); 
+                     },2000)
+            }
+            autoPlay();
+            
